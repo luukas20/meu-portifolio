1ecfe7360a0b63c8235520bf9f438ab403876548
Lucas da Silva Costa <lucas2002.lsc@gmail.com>
2025-08-24 14:21:01 -0300

    Ajustando Cores e Adicionando Pagina de Projetos

--- views.py
+++ views.py
@@ -2,6 +2,10 @@ from main import app
 from flask import render_template
 
 # Rotas
-@app.route('/')
+@app.route('/', endpoint='pagina_home')
 def homepage():
     return render_template("homepage.html")
+
+@app.route('/projetos', endpoint='pagina_projetos')
+def projetos():
+    return render_template("projetos.html")
