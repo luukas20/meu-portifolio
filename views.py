26dff4c5134446cde67a85ea6c031463d4150e5a
Da Silva Costa <lucas.silvacosta@cencosud.com.br>
2025-08-25 23:02:47 -0300

    Ajustes nos Icons Projetos na Pag Principal

--- views.py
+++ views.py
@@ -5,7 +5,3 @@ from flask import render_template
 @app.route('/', endpoint='pagina_home')
 def homepage():
     return render_template("homepage.html")
-
-@app.route('/projetos', endpoint='pagina_projetos')
-def projetos():
-    return render_template("projetos.html")
